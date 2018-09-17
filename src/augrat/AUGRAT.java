4a0d03593101e17bf5ba392dec285b63fbcbcb9b
Madhav Somani <madhavsomani007@gmail.com>
2018-09-16 21:46:02 -0500

    AUG-102: Added Libs Folder

--- src/augrat/AUGRAT.java
+++ src/augrat/AUGRAT.java
@@ -29,11 +29,19 @@ public class AUGRAT extends javax.swing.JFrame {
 
         jLabel1 = new javax.swing.JLabel();
         jSeparator1 = new javax.swing.JSeparator();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         jLabel1.setText("AUGRAT");
+
+        jButton1.setText("GENERAT");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -44,6 +52,10 @@ public class AUGRAT extends javax.swing.JFrame {
                 .addContainerGap(391, Short.MAX_VALUE)
                 .addComponent(jLabel1)
                 .addGap(388, 388, 388))
+            .addGroup(layout.createSequentialGroup()
+                .addGap(392, 392, 392)
+                .addComponent(jButton1)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -52,11 +64,18 @@ public class AUGRAT extends javax.swing.JFrame {
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(533, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 474, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(34, 34, 34))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -94,6 +113,7 @@ public class AUGRAT extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JSeparator jSeparator1;
     // End of variables declaration//GEN-END:variables
--- src/augrat/JavaGenerator.java
+++ src/augrat/JavaGenerator.java
@@ -5,13 +5,53 @@
  */
 package augrat;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 /**
  *
  * @author Maheswari Vasu
  */
 public class JavaGenerator {
-   public static void merger(String args , String args2){
+   public static void merger(String args , String args2) throws FileNotFoundException, IOException{
    
+        // PrintWriter object for file3.txt 
+        PrintWriter pw = new PrintWriter("AUGRAT.java"); 
+          
+        // BufferedReader object for file1.txt 
+        BufferedReader br = new BufferedReader(new FileReader("file1.txt")); 
+          
+        String line = br.readLine(); 
+          
+        // loop to copy each line of  
+        // file1.txt to  file3.txt 
+        while (line != null) 
+        { 
+            pw.println(line); 
+            line = br.readLine(); 
+        } 
+          
+        br = new BufferedReader(new FileReader("file2.txt")); 
+          
+        line = br.readLine(); 
+          
+        // loop to copy each line of  
+        // file2.txt to  file3.txt 
+        while(line != null) 
+        { 
+            pw.println(line); 
+            line = br.readLine(); 
+        } 
+          
+        pw.flush(); 
+          
+        // closing resources 
+        br.close(); 
+        pw.close(); 
+          
        
    }
 }
