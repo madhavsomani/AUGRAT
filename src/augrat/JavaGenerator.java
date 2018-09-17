1a9d647164af375c82ca0a55404147043a54b4cb
Maheswari Vasu <maheswari.vasu4@gmail.com>
2018-09-16 21:14:07 -0500

    AUG-102: Added a java generator class

---
+++ src/augrat/JavaGenerator.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package augrat;
+
+/**
+ *
+ * @author Maheswari Vasu
+ */
+public class JavaGenerator {
+   public static void merger(String args , String args2){
+   
+       
+   }
+}
